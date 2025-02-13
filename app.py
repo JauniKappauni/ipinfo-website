0eca76c98f275f4b9e6152ad790376ce30fb0134
JauniKappauni <social.jauni@gmail.com>
2025-02-13 18:47:44 +0100

    Created simple flask app Added retrieval of ipv4 per request module Added preformatted html tag to output ipv4

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    ipv4 = request.remote_addr
+
+    return render_template("index.html",ipv4=ipv4)
+
+if __name__ == '__main__':
+    app.run(debug=True)
